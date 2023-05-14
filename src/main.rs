0be6f052cfb88519855e7bb54dba4925bddb43bd
Kalidass Mookkaiah <kalidass@gmail.com>
2023-05-14 23:29:17 +0100

    create enum abd use its value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//create enum
+enum IpAddrKind {
+    V4,
+    V6
+}
 fn main() {
     println!("Hello, world!");
+
+    // use enum values
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 }
